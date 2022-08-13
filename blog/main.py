71ec0146e91008e2c474cd2c3d875b96691fef28
Deepak M S <souparnikad40@gmail.com>
2022-08-13 09:53:37 +0530

    successfully executed connection

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from . import schemas
+from . import database
+from . import models
 
 app = FastAPI()
   
+models.Base.metadata.create_all(database.engine)
 # create a blog
 @app.post('/blog')
 def create(request:schemas.Blog):
