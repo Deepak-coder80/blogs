07519c1b1b7656df06f9d76aa05e8923da4e3496
Deepak M S <souparnikad40@gmail.com>
2022-08-13 08:25:10 +0530

    get request 😊

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 # import fast api
 from fastapi import FastAPI
 
+#optional
+from typing import Optional
+
 # create app
 app = FastAPI()
 
 #define the route
-@app.get("/")
+@app.get("/blog")
 # function
-async def index():
-    return {'data':{'name':'Deepak'}};
+def index(limit=10,published:bool=True,sort:Optional[str]= None):
+    if published:
+        return {'data':f'{limit} published blogs from db'}
 
+@app.get('/blog/unpublished')
+async def unpublished():
+    return {'data':'all unpublished blogs'}
 
 #another route
-@app.get("/about")
-def about():
-    return {'data':{'about page'}}
+@app.get("/blog/{id}")
+def show(id:int):
+    return {'data':id}
+
+
+
+# commmets sections
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog wiht id=id 
+    return {'data':'comments'}
