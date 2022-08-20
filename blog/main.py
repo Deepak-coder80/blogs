7a9673d186795c9a1d9291a66b4003285008355e
Deepak M S <souparnikad40@gmail.com>
2022-08-20 22:41:53 +0530

    user table added

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status
 import schemas
 import database
 import models
@@ -16,7 +16,7 @@ def get_db():
         
 
 # create a blog
-@app.post('/blog',status_code=201)
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request:schemas.Blog,db:Session=Depends(get_db)):
     new_blog = models.Blog(title=request.title,body=request.body)
     db.add(new_blog)
@@ -24,15 +24,38 @@ def create(request:schemas.Blog,db:Session=Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}')
+def destroy(id:int,db:Session=Depends(get_db)):
+    blog_d=db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    db.commit()
+    return {'status':'done','content':blog_d}
 
-#get all blogs
+@app.put('/blog/{id}')
+def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
+       db.query(models.Blog).filter(models.Blog.id==id).update({models.Blog.title:request.title})
+
+
+# get all blogs
 @app.get('/blogs')
 def all(db:Session=Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
 
-#get blog by id
+# get blog by id
 @app.get('/blog/{id}')
 def blogById(id,db:Session=Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id==id).first()
     return blog
+
+@app.post('/user')
+def create_user(request: schemas.User,db: Session=Depends(get_db)):
+    new_user =  models.User(name=request.name,email=request.email,password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return  new_user
+
+@app.get('/users')
+def get_all_users(db:Session=Depends(get_db)):
+    user = db.query(models.User).all()
+    return  user;
--- blog/schemas.py
+++ blog/schemas.py
@@ -3,4 +3,11 @@ from pydantic import BaseModel
 
 class Blog(BaseModel):
     title:str
-    body : str 
+    body : str
+
+
+# class for user
+class User(BaseModel):
+    name : str
+    email : str
+    password : str
